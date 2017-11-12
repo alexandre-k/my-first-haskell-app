c10c406e1fc7153f51e8b445c023b285c60b9baf
Alexandre Krispin <k.m.alexandre@gmail.com>
2017-11-12 21:56:12 +0900

    Add features to parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,100 @@
 module Main (main) where
 
+import Options.Applicative hiding (infoParser)
+
+type ItemIndex = Int
+type ItemDescription = Maybe String
+
+data Options = Options FilePath Command deriving Show
+
+data Command =
+  Info
+  | Init
+  | List
+  | Add
+  | View
+  | Update
+  | Remove
+  deriving Show
+
+
+defaultDataPath :: FilePath
+defaultDataPath = "~/.to-do.yaml"
+
+
+infoParser :: Parser Command
+infoParser = pure Info
+
+
+initParser :: Parser Command
+initParser = pure Init
+
+
+listParser :: Parser Command
+listParser = pure List
+
+
+addParser :: Parser Command
+addParser = pure Add
+
+
+viewParser :: Parser Command
+viewParser = pure View
+
+
+updateParser :: Parser Command
+updateParser = pure Update
+
+
+removeParser :: Parser Command
+removeParser = pure Remove
+
+
+commandParser :: Parser Command
+commandParser = subparser $ mconcat
+  [ command "info" (info infoParser (progDesc "Show info"))
+  , command "init" (info initParser (progDesc "Initialize items"))
+  , command "list" (info listParser (progDesc "List all items"))
+  , command "add" (info addParser (progDesc "Add items"))
+  , command "view" (info viewParser (progDesc "View items"))
+  , command "update" (info updateParser (progDesc "Update items"))
+  , command "remove" (info removeParser (progDesc "Remove items"))
+  ]
+
+
+optionsParser :: Parser Options
+optionsParser = Options
+  <$> dataPathParser
+  <*> commandParser
+
+
+dataPathParser :: Parser FilePath
+dataPathParser = strOption $
+  value defaultDataPath
+  <> long "data-path"
+  <> short 'p'
+  <> metavar "DATAPATH"
+  <> help ("path to data file (default " ++ defaultDataPath ++ ")")
+
+
+itemIndexParser :: Parser ItemIndex
+itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "index of item")
+
+
+itemDescriptionValueParser :: Parser String
+itemDescriptionValueParser =
+  strOption (long "desc" <> short 'd' <> metavar "DESCRIPTION" <> help "description")
+
+
+updateItemDescriptionParser :: Parser ItemDescription
+updateItemDescriptionParser =
+  Just <$> itemDescriptionValueParser
+  <|> flag' Nothing (long "clear-desc")
+
+
 main :: IO ()
-main = putStrLn "Hello world"
+main = do
+  options <- execParser (info (optionsParser) (progDesc "To-do list manager"))
+  putStrLn $ "options=" ++ show options
+  itemIndex <- execParser (info (itemIndexParser) (progDesc "To-do list manager"))
+  putStrLn $ "itemIndex=" ++ show itemIndex
